9d7fd959b9f22dd446deedbe6a0a24b32e7d75ff
Damon Tian <damontian@live.com>
2017-05-11 02:33:56 -0400

    updated 3

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: [
+    './src/js/index.jsx'
+  ],
+  output: {
+    filename: "./bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'stage-1', 'react']
+        }
+      }
+    ]
+  }
+}
